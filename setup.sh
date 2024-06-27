39a4a6f18d00d3155dbd0b5c8f6d4b16751112e0
xdevilxyz <129171425+xdevilxyz@users.noreply.github.com>
2024-06-27 12:48:45 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# Install wget and other necessary tools using nix
-nix-env -iA nixpkgs.wget nixpkgs.xz nixpkgs.ffmpeg
-
-# Setup completed message
-echo "Setup completed."
