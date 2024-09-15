8335809a6332e27b372a38958cef9132c76d70b6
xdevilxyz <129171425+xdevilxyz@users.noreply.github.com>
2024-09-15 14:12:39 +0530

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -370,8 +370,8 @@ def handle_link(message):
         )
 
         keyboard = InlineKeyboardMarkup()
-        keyboard.add(InlineKeyboardButton("18+ Bot🤖🔞", url="https://t.me/NightLifeRobot"))
-        keyboard.add(InlineKeyboardButton("More Videos🔞🎥", url="https://t.me/+Jpn6cYnK3r8yYmZk"))
+        keyboard.add(InlineKeyboardButton("18+ Bot🤖( ͜ ㅅ ͜ )", url="https://t.me/NightLifeRobot"))
+        keyboard.add(InlineKeyboardButton("More Videos ( ͜. ㅅ ͜. )yumy🎥", url="https://t.me/+VjA5nTJf8xxkYjk0"))
         keyboard.add(InlineKeyboardButton("Without Token Video🔞", url="https://t.me/+N2SfuzZQ9h45ZGZk"))
         keyboard.add(InlineKeyboardButton("Movie Group🎥", url="https://t.me/+Xs8osVK7iX81Yjc0"))
 
